8accb1c10951f54cba7700d93dabfe6b22846509
陳泰元 <chentaiyuan@chentaiyuans-Mac-mini.local>
2022-05-12 14:26:29 +0800

    [Add]add svm3

--- SVM/main_svm3.py
+++ SVM/main_svm3.py
@@ -6,34 +6,27 @@ from libsvm.svmutil import *
 from scipy.spatial.distance import cdist
 
 from SVM.data_loader.loader import load_all_data
+from SVM.plot.heat_map import pretty_plot
 from SVM.search.grid_search import grid_search, grid_search_custom
 
 
-def custom_kernel(X_1, X_2, g):
-    linear_kernel = X_1 @ X_2.T
-    rbf_kernel = np.exp(-g * cdist(X_1, X_2, 'speuclidean'))
-    combine_kernel = linear_kernel + rbf_kernel
-    index_vector = np.arange(1, len(X_1) + 1)
-    return np.concatenate([index_vector, combine_kernel], axis=1)
+
 
 
 # Data
 X_train, y_train, X_test, y_test = load_all_data()
-custom_kernel(X_train, X_train, )
-training_data = svm_problem(y_train, X_train)
 
 
 
-
-print("_____Radial Basis Function_____")
+print("_____linear + RBF_____")
 kernel = "RBF"
 log2c_parameters = [i - 8 for i in range(0, 18, 2)]
 log2g_parameters = [i - 8 for i in range(0, 18, 2)]
-# log2c_parameters = [1,2]
-# log2g_parameters = [4,5]
+# log2c_parameters = [1]
+# log2g_parameters = [4]
 all_training_acc, best_log2c_parameter, best_log2g_parameter, best_training_acc, best_testing_acc = \
-    grid_search_custom(log2c_parameters, log2g_parameters, kernel_test, training_data, X_test, y_test)
-print("_____Radial Basis Function Result_____")
+    grid_search_custom(log2c_parameters, log2g_parameters, X_train, y_train, X_test, y_test)
+print("_____linear + RBF_____")
 print(f"Best log2c: {best_log2c_parameter}")
 print(f"Best log2g: {best_log2g_parameter}")
 print(f"Best Training Accuracy: {best_training_acc}")
@@ -41,6 +34,7 @@ print(f"Testing Accuracy: {best_testing_acc}")
 print(f"All Training Accuracy: {all_training_acc}")
 with open('all_acc_combine.pickle', 'wb') as f:
     pickle.dump(all_training_acc, f)
-
+pretty_plot('all_acc_combine.pickle', log2c_parameters, log2g_parameters, "linear + RBF")
+print()
 
 
--- SVM/search/grid_search.py
+++ SVM/search/grid_search.py
@@ -1,5 +1,14 @@
 import numpy as np
 from libsvm.svmutil import *
+from scipy.spatial.distance import cdist
+
+
+def custom_kernel(X_1, X_2, g):
+    linear_kernel = X_1 @ X_2.T
+    rbf_kernel = np.exp(-g * cdist(X_1, X_2, 'sqeuclidean'))
+    combine_kernel = linear_kernel + rbf_kernel
+    index_vector = np.arange(1, len(X_1) + 1).reshape(-1, 1)
+    return np.concatenate([index_vector, combine_kernel], axis=1)
 
 
 def grid_search(log2c_parameters, log2g_parameters, kernel, training_data, X_test, y_test):
@@ -40,7 +49,7 @@ def grid_search(log2c_parameters, log2g_parameters, kernel, training_data, X_tes
     return all_training_acc, best_log2c_parameter, best_log2g_parameter, best_training_acc, best_testing_acc
 
 
-def grid_search_custom(log2c_parameters, log2g_parameters, kernel_test, training_data, X_test, y_test):
+def grid_search_custom(log2c_parameters, log2g_parameters, X_train, y_train, X_test, y_test):
     # return grid_matrix, best parameters, best_parameters_testing_acc, best_training_acc
     best_training_acc = -999
     best_log2c_parameter = None
@@ -52,6 +61,8 @@ def grid_search_custom(log2c_parameters, log2g_parameters, kernel_test, training
             print(f"grid searching.... {log2c_parameters[now_focus_log2c], log2g_parameters[now_focus_log2g]}")
             parameter = f"-q -t 4 -v 3 -c {2 ** log2c_parameters[now_focus_log2c]} -g {2 ** log2g_parameters[now_focus_log2g]}"
             print(parameter)
+            new_kernel_train = custom_kernel(X_train, X_train, 2 ** log2g_parameters[now_focus_log2g])
+            training_data = svm_problem(y_train, new_kernel_train, isKernel=True)
             result = svm_train(training_data, parameter)
             all_training_acc[now_focus_log2c, now_focus_log2g] = result
             if best_training_acc < result:
@@ -60,7 +71,12 @@ def grid_search_custom(log2c_parameters, log2g_parameters, kernel_test, training
                 best_log2g_parameter = log2g_parameters[now_focus_log2g]
     # testing process
     parameter = f"-q -t 4 -c {2 ** best_log2c_parameter} -g {2 ** best_log2g_parameter}"
-    best_model = svm_train(training_data, parameter)
-    p_labels, p_acc, p_vals = svm_predict(y_test, X_test, best_model)
+
+    new_kernel_train_testing = custom_kernel(X_train, X_train, 2 ** best_log2g_parameter)
+    train_testing_data = svm_problem(y_train, new_kernel_train_testing, isKernel=True)
+    best_model = svm_train(train_testing_data, parameter)
+
+    new_kernel_test = custom_kernel(X_test, X_train, 2 ** best_log2g_parameter)
+    p_labels, p_acc, p_vals = svm_predict(y_test, new_kernel_test, best_model)
     best_testing_acc = p_acc[0]
     return all_training_acc, best_log2c_parameter, best_log2g_parameter, best_training_acc, best_testing_acc
