132868cecd946efdafb065fcfdee49cc3b62469b
陳泰元 <chentaiyuan@chentaiyuans-Mac-mini.local>
2022-05-12 13:06:29 +0800

    [Add]add RBF svm 2 and svm3

--- SVM/main_svm_2.py
+++ SVM/main_svm_2.py
@@ -34,21 +34,41 @@ testing_data = svm_problem(y_test, X_test)
 # pretty_plot('all_acc_linear.pickle', log2c_parameters, log2g_parameters, "linear")
 # print()
 
-print("_____Polynomial_____")
-kernel = "polynomial"
+# print("_____Polynomial_____")
+# kernel = "polynomial"
+# log2c_parameters = [i - 8 for i in range(0, 18, 2)]
+# log2g_parameters = [i - 8 for i in range(0, 18, 2)]
+# # log2c_parameters = [1,2,3]
+# # log2g_parameters = [4,5,6]
+# all_training_acc, best_log2c_parameter, best_log2g_parameter, best_training_acc, best_testing_acc = \
+#     grid_search(log2c_parameters, log2g_parameters, kernel, training_data, X_test, y_test)
+# print("_____Polynomial Result_____")
+# print(f"Best log2c: {best_log2c_parameter}")
+# print(f"Best log2g: {best_log2g_parameter}")
+# print(f"Best Training Accuracy: {best_training_acc}")
+# print(f"Testing Accuracy: {best_testing_acc}")
+# print(f"All Training Accuracy: {all_training_acc}")
+# with open('all_acc_polynomial.pickle', 'wb') as f:
+#     pickle.dump(all_training_acc, f)
+# pretty_plot('all_acc_polynomial.pickle', log2c_parameters, log2g_parameters, "polynomial")
+# print()
+
+
+print("_____Radial Basis Function_____")
+kernel = "RBF"
 log2c_parameters = [i - 8 for i in range(0, 18, 2)]
 log2g_parameters = [i - 8 for i in range(0, 18, 2)]
-# log2c_parameters = [1,2,3]
-# log2g_parameters = [4,5,6]
+# log2c_parameters = [1,2]
+# log2g_parameters = [4,5]
 all_training_acc, best_log2c_parameter, best_log2g_parameter, best_training_acc, best_testing_acc = \
     grid_search(log2c_parameters, log2g_parameters, kernel, training_data, X_test, y_test)
-print("_____Polynomial Result_____")
+print("_____Radial Basis Function Result_____")
 print(f"Best log2c: {best_log2c_parameter}")
 print(f"Best log2g: {best_log2g_parameter}")
 print(f"Best Training Accuracy: {best_training_acc}")
 print(f"Testing Accuracy: {best_testing_acc}")
 print(f"All Training Accuracy: {all_training_acc}")
-with open('all_acc_polynomial.pickle', 'wb') as f:
+with open('all_acc_rbf.pickle', 'wb') as f:
     pickle.dump(all_training_acc, f)
-pretty_plot('all_acc_polynomial.pickle', log2c_parameters, log2g_parameters, "polynomial")
+pretty_plot('all_acc_rbf.pickle', log2c_parameters, log2g_parameters, "RBF")
 print()
--- SVM/search/grid_search.py
+++ SVM/search/grid_search.py
@@ -38,3 +38,29 @@ def grid_search(log2c_parameters, log2g_parameters, kernel, training_data, X_tes
     p_labels, p_acc, p_vals = svm_predict(y_test, X_test, best_model)
     best_testing_acc = p_acc[0]
     return all_training_acc, best_log2c_parameter, best_log2g_parameter, best_training_acc, best_testing_acc
+
+
+def grid_search_custom(log2c_parameters, log2g_parameters, kernel_test, training_data, X_test, y_test):
+    # return grid_matrix, best parameters, best_parameters_testing_acc, best_training_acc
+    best_training_acc = -999
+    best_log2c_parameter = None
+    best_log2g_parameter = None
+
+    all_training_acc = np.zeros((len(log2c_parameters), len(log2g_parameters)))
+    for now_focus_log2c in range(len(log2c_parameters)):
+        for now_focus_log2g in range(len(log2g_parameters)):
+            print(f"grid searching.... {log2c_parameters[now_focus_log2c], log2g_parameters[now_focus_log2g]}")
+            parameter = f"-q -t 4 -v 3 -c {2 ** log2c_parameters[now_focus_log2c]} -g {2 ** log2g_parameters[now_focus_log2g]}"
+            print(parameter)
+            result = svm_train(training_data, parameter)
+            all_training_acc[now_focus_log2c, now_focus_log2g] = result
+            if best_training_acc < result:
+                best_training_acc = result
+                best_log2c_parameter = log2c_parameters[now_focus_log2c]
+                best_log2g_parameter = log2g_parameters[now_focus_log2g]
+    # testing process
+    parameter = f"-q -t 4 -c {2 ** best_log2c_parameter} -g {2 ** best_log2g_parameter}"
+    best_model = svm_train(training_data, parameter)
+    p_labels, p_acc, p_vals = svm_predict(y_test, X_test, best_model)
+    best_testing_acc = p_acc[0]
+    return all_training_acc, best_log2c_parameter, best_log2g_parameter, best_training_acc, best_testing_acc
